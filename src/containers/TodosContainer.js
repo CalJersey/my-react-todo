c61e760e6908dba7c3723deb62ec378096cfa5a6
CalJersey <crippe@gmail.com>
2017-08-02 14:31:12 -0700

    Sprint 2: todos container

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class TodosContainer extends Component {
+render() {
+  return (
+    <div className='todosContainer'>
+      <h2>This is the Todos Container</h2>
+    </div>
+    )
+  }
+}
+
+export default TodosContainer
